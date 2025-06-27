3420a8812d78035eb9a2ed3596132f90818a04b6
Altaf24 <tambolialtaf1419@gmail.com>
2025-06-27 09:47:15 +0530

    added a signup and login in backend

--- server/app.js
+++ server/app.js
@@ -1,23 +1,30 @@
-const express = require('express');
-const connectDB =  require('./config/database');
-const  app = express();
-require('dotenv').config();
-// const cookieParser = require('cookie-parser');
-const cors = require('cors');
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDb from './config/database.js'
+import cookieParser from 'cookie-parser'
+import authRoutes from './routes/authRoutes.js'
+dotenv.config()
+import cors from "cors"
+
+let app = express();
 
 
-// app.use(cors({
-//     origin: 'http://localhost:5173',
-//     credentials: true,
-// }));
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser('GCC@LTD$2024')); // Use same secret as JWT
+app.use(cookieParser('GCC@LTD$2024')); // Use same secret as JWT
+
+
+app.use('/api/auth', authRoutes);
 
 
 
 
-connectDB().then(()=>{
+connectDb().then(()=>{
     console.log('DB connected to mongodb');
     app.listen(34567,()=>{
         console.log('server is running on port 34567....');
